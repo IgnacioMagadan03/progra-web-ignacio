8f253e46852c8ed91ab76b32d3bccadb2ff1b27f
IgnacioMagadan03 <imagadan@itba.edu.ar>
2024-06-17 17:27:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 
-// const fechaObjetivo = new Date("2025-05-10T10:30:00");
+const fechaObjetivo = new Date("2025-05-10T10:30:00");
 
-// const dias = document.getElementById("dias");
-// const horas = document.getElementById("horas");
-// const minutos = document.getElementById("minutos");
-// const segundos = document.getElementById("segundos");
+const dias = document.getElementById("dias");
+const horas = document.getElementById("horas");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
 
-// function actualizarContador() {
-//     const ahora = new Date();
-//     const tiempoRestante = fechaObjetivo - ahora;
+function actualizarContador() {
+    const ahora = new Date();
+    const tiempoRestante = fechaObjetivo - ahora;
 
-//     if (tiempoRestante <= 0) {
-//         clearInterval(intervaloActualizacion);
-//         dias.textContent = "00";
-//         horas.textContent = "00";
-//         minutos.textContent = "00";
-//         segundos.textContent = "00";
+    if (tiempoRestante <= 0) {
+        clearInterval(intervaloActualizacion);
+        dias.textContent = "00";
+        horas.textContent = "00";
+        minutos.textContent = "00";
+        segundos.textContent = "00";
         
-//         lanzarConfeti();
+        lanzarConfeti();
         
-//         return; 
-//     }
+        return; 
+    }
 
-//     const diasRestantes = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
-//     const horasRestantes = Math.floor((tiempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const minutosRestantes = Math.floor((tiempoRestante % (1000 * 60 * 60)) / (1000 * 60));
-//     const segundosRestantes = Math.floor((tiempoRestante % (1000 * 60)) / 1000);
+    const diasRestantes = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
+    const horasRestantes = Math.floor((tiempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosRestantes = Math.floor((tiempoRestante % (1000 * 60 * 60)) / (1000 * 60));
+    const segundosRestantes = Math.floor((tiempoRestante % (1000 * 60)) / 1000);
 
-//     dias.textContent = diasRestantes.toString().padStart(2, "0");
-//     horas.textContent = horasRestantes.toString().padStart(2, "0");
-//     minutos.textContent = minutosRestantes.toString().padStart(2, "0");
-//     segundos.textContent = segundosRestantes.toString().padStart(2, "0");
-// }
-// const intervaloActualizacion = setInterval(actualizarContador, 1000);
+    dias.textContent = diasRestantes.toString().padStart(2, "0");
+    horas.textContent = horasRestantes.toString().padStart(2, "0");
+    minutos.textContent = minutosRestantes.toString().padStart(2, "0");
+    segundos.textContent = segundosRestantes.toString().padStart(2, "0");
+}
+const intervaloActualizacion = setInterval(actualizarContador, 1000);
 
 
 var carritoVisible =false;
